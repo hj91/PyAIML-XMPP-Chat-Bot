1cea57f4fde18366cf8616c0c80ec340aa51d986
Harshad Joshi <firewalrus@gmail.com>
2011-05-06 19:58:22 +0530

    initial commit

---
+++ xmpp_aiml.py
@@ -0,0 +1,73 @@
+# This programs is basically a chatbot
+# Uses PyAIML and xmpppy
+#
+# Author       - Harshad Joshi
+# Date         - May 6, 2011
+#
+# Requirements - XMPP chat server (openfire)
+#              - Python 2.5 with xmpp and PyAIML library.
+#
+# Features     - Unicode enabled
+#
+# ToDo         - Bot gets kicked off after being idle for 5 or 6 minutes. 
+#              - Needs to send 'KeepALive' packet.
+#              - After getting logged in, it posts 'None' as the first message. Need to remove it.
+#	       - Dosent work with gtalk. i dont know the reason.
+#              - should ask for it once and store it in the backend.
+
+
+
+import sys
+
+import time
+import datetime
+import xml.sax.saxutils
+import xmlrpclib
+import xmpp
+import codecs
+import aiml
+
+
+user='user@gmail.com'
+passwd='passwd'
+server='gmail.com'
+
+
+# the heart and brain of bot...
+
+k = aiml.Kernel()
+
+k.learn("std-startup.xml")
+
+k.respond("load aiml b")
+
+
+class Bot:
+	def message_handler(connect_object,message_node):
+		command1=str(unicode(message_node.getBody()).encode('utf-8'))
+		command2=str(message_node.getFrom().getStripped())
+				
+		#the fun begins from this place..
+		connect_object.send(xmpp.Message(command2,(k.respond(command1))))				
+	
+		
+	jid=xmpp.JID(user)
+	connection=xmpp.Client(server)
+	connection.connect()
+	result=connection.auth(jid.getNode(),passwd)
+	connection.RegisterHandler('message',message_handler,"")
+	connection.sendInitPresence()
+        press = xmpp.Presence()
+        press.setStatus("Hi from bot...i will chat with you and tell you new things you might like")
+        connection.send(press)
+
+	while (1):
+		connection.Process(1)
+
+a=Bot()
+a
+
+	
+
+
+
